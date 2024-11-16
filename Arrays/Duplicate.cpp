9eb695afb0f6a3722c2f2d44b5689248a12f3f97
AvichalDwivedi2205 <150160561+AvichalDwivedi2205@users.noreply.github.com>
2024-11-16 21:52:39 +0530

    Even better approach

--- Arrays/Duplicate.cpp
+++ Arrays/Duplicate.cpp
@@ -25,10 +25,21 @@ public:
             }
             nums[index]*=-1;
         }
+        for(int i=0;i<n;i++){
+            if(nums[i]<0)
+            nums[i]*=-1;
+        }
         return ans;
     }
 
+    int PositioningMethod(vector<int>& nums){
+        while(nums[0] != nums[nums[0]]){
+            swap(nums[0], nums[nums[0]]);
+        }
+        return nums[0];
+    }
+
     int findDuplicate(vector<int>& nums) {
-        return betterApproach(nums);
+        return PositioningMethod(nums);
     }
 };
