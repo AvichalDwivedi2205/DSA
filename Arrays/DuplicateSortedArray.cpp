52691f87b5070b6c90c8f6ebaa52ab89f525e7ab
AvichalDwivedi2205 <150160561+AvichalDwivedi2205@users.noreply.github.com>
2024-11-17 22:44:30 +0530

    Remove duplicate from sorted array

---
+++ Arrays/DuplicateSortedArray.cpp
@@ -0,0 +1,13 @@
+#include <vector>
+using namespace std;
+
+int removeDuplicates(vector<int>& nums) {
+        int c = 1;
+        for (int i=1; i<nums.size(); i++){
+           if(nums[i] != nums[i - 1]){
+                nums[c] = nums[i];
+                c++;
+            }
+        }
+        return c;
+    }
