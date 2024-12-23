f4b705f341d72f18371a8a5c92441347dec1aaad
AvichalDwivedi2205 <150160561+AvichalDwivedi2205@users.noreply.github.com>
2024-12-23 12:11:04 +0530

    Valid Palindrome 2 leetcode

---
+++ Strings/ValidPalindrome2.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include<string.h>
+using namespace std;
+
+class Solution {
+public:
+    
+    bool checkPalindrome(string s, int i, int j){
+        while(i<=j){
+            if(s[i]==s[j]){
+                i++;
+                j--;
+            }else{
+                return false;
+            }
+        }
+        return true;
+    }
+
+    bool validPalindrome(string s) {
+        int i=0, j=s.length()-1;
+        while(i<j){
+            if(s[i]==s[j]){
+                i++;
+                j--;
+            }else{
+                bool ans1=checkPalindrome(s,i+1,j);
+                bool ans2=checkPalindrome(s,i,j-1);
+                return ans1 || ans2;
+            }
+        }
+        return true;
+    }
+};
