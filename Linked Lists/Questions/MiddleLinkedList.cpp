2ffc02ea067d296a9693351d285e482d88a51244
AvichalDwivedi2205 <150160561+AvichalDwivedi2205@users.noreply.github.com>
2024-12-25 15:56:41 +0530

    Finding middle of a linkedlist

---
+++ Linked Lists/Questions/MiddleLinkedList.cpp
@@ -0,0 +1,23 @@
+# include<iostream>
+using namespace std;
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+class Solution {
+public:
+    ListNode* middleNode(ListNode* head) {
+        ListNode* temp=head;
+        ListNode* middle=head;
+        while(temp!=NULL && temp->next!=NULL){
+            middle=middle->next;
+            temp=(temp->next)->next;
+        }
+        return middle;
+    }
+};
