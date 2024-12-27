94f17ff1b1adbff88b307ede025836a4ff65dc90
AvichalDwivedi2205 <150160561+AvichalDwivedi2205@users.noreply.github.com>
2024-12-27 20:51:33 +0530

    Previous smaller element

---
+++ Stacks/Questions/PreviousSmallerElement.cpp
@@ -0,0 +1,23 @@
+# include<iostream>
+# include <stack>
+# include <vector>
+using namespace std;
+
+class Solution {
+  public:
+    vector<int> immediateSmaller(vector<int>& arr) {
+        //  code here
+        stack <int> st;
+        vector<int> ans(arr.size());
+        st.push(-1);
+        for(int i=0;i<arr.size();i++){
+            int curr=arr[i];
+            while(st.top()>=curr){
+                st.pop();
+            }
+            ans[i]=st.top();
+            st.push(curr);
+        }
+        return ans;
+    }
+};
