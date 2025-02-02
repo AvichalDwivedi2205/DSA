a1290d2f3e6d2354ef6f35b937411f8cc05492c2
AvichalDwivedi2205 <150160561+AvichalDwivedi2205@users.noreply.github.com>
2025-02-02 17:39:22 +0530

    Adding dfs stuff

--- Graphs/Questions/UndirectedGraphCycle.cpp
+++ Graphs/Questions/UndirectedGraphCycle.cpp
@@ -1,9 +1,14 @@
 #include<vector>
 # include <unordered_map>
+# include <iostream>
 # include <queue>
 using namespace std;
 
-bool solve(vector<vector<int>>& adj, int src, unordered_map<int, bool> &vis){
+class Solution {
+  public:
+    // Function to detect cycle in an undirected graph.
+    
+    bool solveBFS(vector<vector<int>>& adj, int src, unordered_map<int, bool> &vis){
         queue<int> q;
         unordered_map<int, int> parent;
         // push the src node
@@ -29,14 +34,59 @@ bool solve(vector<vector<int>>& adj, int src, unordered_map<int, bool> &vis){
         return false;
     }
     
+    bool solveDFS(vector<vector<int>>& adj, int src, unordered_map<int, bool> &vis, int parent){
+        vis[src] = true;
+        
+        for(auto nbr : adj[src]){
+            if(nbr==parent) continue;
+            if(!vis[nbr]){
+                bool ans = solveDFS(adj, nbr, vis, src);
+                if(ans) return true;
+            } else{
+                return true;
+            }
+        }
+        return false;
+    }
+
+    
     bool isCycle(vector<vector<int>>& adj) {
         // Code here
         unordered_map<int, bool> vis;
         for (int i=0;i<adj.size(); i++){
             if(!vis[i]){
-                bool ans = solve(adj, i, vis);
+                bool ans = solveDFS(adj, i, vis, -1);
                 if(ans==true) return true;
             }
         }
         return false;
     }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int tc;
+    cin >> tc;
+    while (tc--) {
+        int V, E;
+        cin >> V >> E;
+        vector<vector<int>> adj(V);
+        for (int i = 0; i < E; i++) {
+            int u, v;
+            cin >> u >> v;
+            adj[u].push_back(v);
+            adj[v].push_back(u);
+        }
+        Solution obj;
+        bool ans = obj.isCycle(adj);
+        if (ans)
+            cout << "1\n";
+        else
+            cout << "0\n";
+
+        cout << "~"
+             << "\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
